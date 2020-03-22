fb5bc080de354c2f938964fed38ef463c4ad7f92
Alina Andreeva <andreeva.a.wezom@gmail.com>
2020-03-22 14:54:25 +0200

    fix

---
+++ public/build/precache-manifest.64f7d1c71d3597a1973701c6b81c379f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1315ee1f729a460298aefab7ac0d06c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ceee314be4bb631e5fd",
+    "url": "/static/css/main.c9be5be6.chunk.css"
+  },
+  {
+    "revision": "f948bd798fd4eec1cb2f",
+    "url": "/static/js/2.29306541.chunk.js"
+  },
+  {
+    "revision": "37b5fb9fa73e0688add42527cf5813eb",
+    "url": "/static/js/2.29306541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ceee314be4bb631e5fd",
+    "url": "/static/js/main.943601b1.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  }
+]);
